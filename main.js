e39e52b1d72ad67a2c1fded4d12218f37098842e
edahigure <edahigure@gmail.com>
2022-10-17 17:03:36 -0500

    add menu page

---
+++ main.js
@@ -0,0 +1,8 @@
+let div = document.createElement('div');
+div.className = 'mobile-menu';
+div.id = 'content';
+div.innerHTML = '<p>Create Element example</p>';
+
+document.body.appendChild(div);
+
+
