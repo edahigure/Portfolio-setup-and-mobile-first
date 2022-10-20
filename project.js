44cf1d57cd41a153459c79d55e78058f740631a8
edahigure <edahigure@gmail.com>
2022-10-20 09:11:11 -0500

    adding popup

---
+++ project.js
@@ -0,0 +1,233 @@
+let project_array = [
+    
+
+     {
+        name: 'card-0',
+        description: `Here goes description`,        
+        featured: 'feature-2',
+        image: './images/img-work-desktop.png',
+        technologies: [ 'css','html','bootstrap','Ruby'],
+        link_to_live_version: '#',
+        link_to_source: '#',
+        
+     },
+     {
+        name: 'card-1',
+        description: `No description`,        
+        featured: 'feature-2',
+        image: './images/img-work-desktop.png',
+        technologies: [ 'css','html','bootstrap','Ruby'],
+        link_to_live_version: '#',
+        link_to_source: '#',
+        title_post:'Profesional Art Printing Data',
+        supporting_text: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        type_image: 'image-card'
+     },
+     {
+        name: 'card-2',
+        description: `No description`,        
+        featured: 'feature-2',
+        image: './images/img-work-desktop.png',
+        technologies: [ 'css','html','bootstrap','Ruby'],
+        link_to_live_version: '#',
+        link_to_source: '#',
+        title_post:'Data Dashboard Healthcare',
+        supporting_text: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        type_image: 'image-card-B'
+     },
+     {
+        name: 'card-3',
+        description: `No description`,        
+        featured: 'feature-2',
+        image: './images/img-work-desktop.png',
+        technologies: [ 'css','html','bootstrap','Ruby'],
+        link_to_live_version: '#',
+        link_to_source: '#',
+        title_post:'Data Dashboard Healthcare',
+        supporting_text: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        type_image: 'image-card-B'
+     },
+     {
+        name: 'card-4',
+        description: `No description`,        
+        featured: 'feature-2',
+        image: './images/img-work-desktop.png',
+        technologies: [ 'css','html','bootstrap','Ruby'],
+        link_to_live_version: '#',
+        link_to_source: '#',
+        title_post:'Website Protfolio',
+        supporting_text: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        type_image: 'image-card-C'
+     },
+     {
+        name: 'card-5',
+        description: `No description`,        
+        featured: 'feature-2',
+        image: './images/img-work-desktop.png',
+        technologies: [ 'css','html','bootstrap','Ruby'],
+        link_to_live_version: '#',
+        link_to_source: '#',
+        title_post:'Website Protfolio',
+        supporting_text: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        type_image: 'image-card-C'
+     }
+
+ ];
+
+
+let bigString =`
+    <header class="title">
+        <div class="text" id="portfolio">My Recent Works</div>        
+    </header>        
+    
+    <div class="container-duo-A" id='id-container-duo-A'>
+
+        <img src="./images/img-work-desktop.png" class="image-work">
+
+        <div class="extra-wrapper" > 
+
+            <h3 class="title-multi" >
+                Multi-Post Stories
+            </h3>
+    
+            <p class="works-description">
+                A daily selection of privately personalized 
+                    reads; no accounts or sign-ups required. 
+                    has been the industry's standard dummy text
+                    ever since the 1500s, when an unknown
+                    printer took a standard dummy text.
+            </p>
+    
+            
+            <ul class="button-container-A">
+                <li class="button1">css</li>
+                <li class="button1">html</li>
+                <li class="button1">bootstrap</li>
+                <li class=
+                "button1">Ruby</li> 
+                <button class="button2" id = "orange-button-A" >See Project</button>                                  
+            </ul>  
+        </div>            
+
+    </div>
+    <div class="wrapper-cards">
+
+`;
+
+for(let i = 0 ; i<project_array.length; i++ ) {
+    console.log(i);
+    
+    
+    if(i==0){
+        bigString += ''; 
+        bigString += `        <div class="card-${i}">
+            <div class="up"></div>
+            <div class="down"> See project</div>
+        </div>`; 
+    }
+    if(i>0){
+        bigString += `
+        <div class="card-${i}">
+            
+            <div class="${project_array[i].type_image}">  
+    
+                 <h2 class="title-post"> ${project_array[i].title_post} </h2>
+                 <h3 class="supporting-text">${project_array[i].supporting_text}</h3>
+    
+                 <div class="button-container">                         
+                 <button type="button" class="button3">${project_array[i].technologies[1]}</button>
+                 <button type="button" class="button3">${project_array[i].technologies[2]}</button>
+                 <button type="button" class="button3">${project_array[i].technologies[3]}</button>                
+                 </div>       
+                    
+            </div>  
+
+            <div class="orange-button" id="orange-button-${i}">
+                See Project
+            </div>        
+        </div>
+    `;        
+    }    
+}
+
+bigString += '</div>\n';
+
+console.log(bigString);
+
+let wrapper_works = document.querySelector('.wrapper-works');
+wrapper_works.innerHTML = bigString ;
+
+
+
+let card_intro = document.getElementById('orange-button-A');
+
+function show_project_intro() {
+    
+    let pop_up_window = document.querySelector('.pop-up-window');
+
+    pop_up_window.innerHTML=`
+    <div class="overlay">           
+    </div>
+    
+    <div class="container-pop-up">
+        
+         <div class="container-duo-pop">
+
+            <h3 class="title-multi" >
+            Multi-Post Stories
+            </h3>
+
+            <div class="cross">
+            <a href="#" class="close_item"><img src="./images/cross_2.png"></a>
+            </div>               
+
+         </div>
+
+         
+        <div class="extra-wrapper" > 
+            
+                    
+            <ul class="button-container-popup">
+                <li class="button1">html</li>
+                <li class="button1">Bootstrap</li>
+                <li class="button1">Ruby on rails</li>                                
+            </ul>  
+
+            <img src="./images/Snapshoot Portfolio.png" class="image-work">
+
+            <p class="works-description-popup">
+            Lorem Ipsum is simply dummy text of the printing and
+            typesetting industry. Lorem Ipsum has been the 
+            industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and 
+            scrambled it to make a type specimen book. It
+            has survived not only five centuries, but
+            also the leap into electronic typesetting, 
+            remaining essent
+            </p>
+            
+
+        </div>            
+        <div class="button-duo-popup">
+            <button class="orange-button-popup">See Live</button>
+            <button class="orange-button-popup">See Source</button>
+        <div>
+
+
+    </div>
+    
+    `;
+//    let overlay = document.createElement('div');
+//       overlay.className = 'overlay';          
+//       pop_up_window.appendChild(overlay);    
+
+    let overlay = document.querySelector('.overlay');
+    let container_pop_up = document.querySelector('.container-pop-up');
+    overlay.style.display = 'flex';     
+    container_pop_up.style.display = 'flex'; 
+   
+}
+card_intro.addEventListener('click',show_project_intro);
+
+
+
