3a83ffac6655b1cd5771a62737548c9f42deb77b
edahigure <edahigure@gmail.com>
2022-10-22 19:46:50 -0500

    adding local storage

--- local_storage.js
+++ local_storage.js
@@ -1,26 +1,18 @@
 
-<<<<<<< HEAD
 
 
 
-=======
->>>>>>> 586bbc506fab3a6dc3fa676b09c4530edbea84d7
 let data_form = {
     name: '',
     email:'',
     message:''
 }
   
-<<<<<<< HEAD
-=======
-
->>>>>>> 586bbc506fab3a6dc3fa676b09c4530edbea84d7
 let ele_name=document.getElementById("name");
 let ele_email=document.getElementById("email");
 let ele_msg=document.getElementById("msg");
 
 
-<<<<<<< HEAD
 if(!localStorage.getItem('name')) {    
     localStorage.setItem('name', ele_name.value);
     data_form.name = ele_name.value;
@@ -48,27 +40,11 @@ if(!localStorage.getItem('msg')) {
 
 
 
-=======
-if(!localStorage.getItem('name')) {
-//populateStorage();
-//console.log("no name in local storage ");
-localStorage.setItem('name', ele_name.value);
-data_form.name = ele_name.value;
-} else {
-//setData_from_storage();
-//console.log(" name in local storage: ",localStorage.getItem('name'));
-data_form.name = localStorage.getItem('name');
-ele_name.value = data_form.name;
-}
-
-
->>>>>>> 586bbc506fab3a6dc3fa676b09c4530edbea84d7
 function name_changed() {
 console.log(ele_name.value);   
 localStorage.setItem('email', ele_name.value);
 data_form.name= ele_name.value;  
 }
-<<<<<<< HEAD
 ele_name.addEventListener("input",  name_changed );
 
 
@@ -86,11 +62,6 @@ function msg_changed() {
     data_form.msg= ele_msg.value;  
 }
 ele_msg.addEventListener("input",  msg_changed );
-=======
-email.addEventListener("input",  name_changed );
-
-
->>>>>>> 586bbc506fab3a6dc3fa676b09c4530edbea84d7
 
 
 
